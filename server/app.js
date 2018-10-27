e0fe610db3451d0e78a92ded0e8837be501ade49
Cameron Braverman <bravermancj@gmail.com>
2018-10-26 22:19:48 -0400

    added boilerplate server code

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Wassup');
+});
+
+if (process.env.NODE_ENV === 'development') {
+    // only use in development
+    app.use(errorHandler());
+} else {
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.status(500).send('Server Error');
+    });
+}
+
+app.listen(8000, () => {
+    console.log('Server running...');
+});
